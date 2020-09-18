91c39cc21ef021f362e2c4bf206a37f76c104897
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-09-18 16:39:11 +0900

    finish class inherit

--- product.rb
+++ product.rb
@@ -1,13 +1,28 @@
 class Product
-    attr_reader :name, :price
+    # attr_reader :name, :price
 
-    def initialize(name, price)
-        @name = name
-        @price = price
+    # def initialize(name, price)
+    #     @name = name
+    #     @price = price
+    # end
+
+    # product = Product.new("great move", 1000)
+    # product.name
+    # product.price
+    private
+    
+    # def name
+    #     "A greate product"
+    # end
+
+    def to_s
+        "name: #{name}" #DVDクラスでオーバーライド＝＞　A greate　movieになるとは限らない
+    end
+
+    private
+
+    def name
+        "A grate movie"
     end
 
 end
-
-product = Product.new("great move", 1000)
-product.name
-product.price
